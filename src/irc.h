eb0bd829c27fee311c6f7baa6566f4f8229d523e
electra01 <eren@hotmail.ca>
2017-03-29 05:36:41 +0300

    Add files via upload

---
+++ src/irc.h
@@ -0,0 +1,12 @@
+// Copyright (c) 2009-2016 Satoshi Nakamoto
+// Copyright (c) 2009-2016 The Bitcoin developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+#ifndef ELECTRA_IRC_H
+#define ELECTRA_IRC_H
+
+void ThreadIRCSeed(void* parg);
+
+extern int nGotIRCAddresses;
+
+#endif
---
+++ src/txdb.h
@@ -0,0 +1,11 @@
+// Copyright (c) 2009-2016 Satoshi Nakamoto
+// Copyright (c) 2009-2016 The Bitcoin developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file license.txt or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef ELECTRA_TXDB_H
+#define ELECTRA_TXDB_H
+
+#include "txdb-leveldb.h"
+
+#endif  // ELECTRA_TXDB_H
