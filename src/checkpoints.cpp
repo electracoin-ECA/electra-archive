ce3f7af413d693ca536a0377cd8ec3661bd6be9c
bumbacoin <bumbacoin@gmail.com>
2017-06-23 18:18:20 +0800

    Update checkpoints.cpp
    fix checkpoint typo

--- src/checkpoints.cpp
+++ src/checkpoints.cpp
@@ -26,9 +26,9 @@ namespace Checkpoints
         boost::assign::map_list_of
         ( 0,      hashGenesisBlock )
         ( 10, uint256("000000510a92dcd0364d79ace286a8d44c060f96eff907f649c00b120901fa69"))
-        ( 18110, uint256("000000000011a409fad375babe9e521e31095de1707e261ca783242c95827593"))
-        ( 18116, uint256("3f90dc9f885df3e167a4ad3a665ffc5282e3d4cd2291b616abc3ddc8b623ffdc"))
-
+        ( 11810, uint256("000000000011a409fad375babe9e521e31095de1707e261ca783242c95827593"))
+        ( 11816, uint256("3f90dc9f885df3e167a4ad3a665ffc5282e3d4cd2291b616abc3ddc8b623ffdc"))
+        ( 11920, uint256("000000000949e8afa1fc49b5faf29bdf3b7ed39ea7f230637fafbb7cf7b04956"))
     ;
 
 
