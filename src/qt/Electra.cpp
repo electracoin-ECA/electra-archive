df4467f13767c3067cffd7ab62a014607b1a02ac
Alejandro Vera <xafilox@gmail.com>
2018-01-20 01:28:11 +0100

    Allow build without GUI using GUI=0
    Use qmake GUI=0 to generate a Makefile that will build without GUI.

--- src/qt/Electra.cpp
+++ src/qt/Electra.cpp
@@ -110,7 +110,7 @@ static void handleRunawayException(std::exception *e)
     exit(1);
 }
 
-#ifndef ELECTRA_QT_TEST
+#if !defined(ELECTRA_QT_TEST) && defined(QT_GUI)
 int main(int argc, char *argv[])
 {
     // Do this early as we don't want to bother initializing if we are just calling IPC
