9c40be00f4c95288e82967bdfeb9550f03de9e2f
David <dstoneham.93@gmail.com>
2018-06-28 12:48:39 +1000

    RPC updates for staking wallet

--- src/rpcwallet.cpp
+++ src/rpcwallet.cpp
@@ -148,6 +148,10 @@ Value getnewaddress(const Array& params, bool fHelp)
             "If [account] is specified (recommended), it is added to the address book "
             "so payments received with the address will be credited to [account].");
 
+    // DS: fix GetAccountAddress not commiting new account to wallet file
+    CWalletDB walletdb(pwalletMain->strWalletFile);
+    CAccount account = CAccount();
+
     // Parse the account first so we don't generate a key if there's an error
     string strAccount;
     if (params.size() > 0)
@@ -156,15 +160,15 @@ Value getnewaddress(const Array& params, bool fHelp)
     if (!pwalletMain->IsLocked())
         pwalletMain->TopUpKeyPool();
 
+    // DS: fix GetAccountAddress not commiting new account to wallet file
     // Generate a new key that is added to wallet
-    CPubKey newKey;
-    if (!pwalletMain->GetKeyFromPool(newKey, false))
-        throw JSONRPCError(RPC_WALLET_KEYPOOL_RAN_OUT, "Error: Keypool ran out, please call keypoolrefill first");
-    CKeyID keyID = newKey.GetID();
+    if (!pwalletMain->GetKeyFromPool(account.vchPubKey))
+            throw JSONRPCError(RPC_WALLET_KEYPOOL_RAN_OUT, "Error: Keypool ran out, please call keypoolrefill first");
 
-    pwalletMain->SetAddressBookName(keyID, strAccount);
+    pwalletMain->SetAddressBookName(account.vchPubKey.GetID(), strAccount);
+    walletdb.WriteAccount(strAccount, account);
 
-    return CElectraAddress(keyID).ToString();
+    return CElectraAddress(account.vchPubKey.GetID()).ToString();
 }
 
 
@@ -241,13 +245,10 @@ Value setaccount(const Array& params, bool fHelp)
     if (params.size() > 1)
         strAccount = AccountFromValue(params[1]);
 
-    // Detect when changing the account of an address that is the 'unused current key' of another account:
-    if (pwalletMain->mapAddressBook.count(address.Get()))
-    {
-        string strOldAccount = pwalletMain->mapAddressBook[address.Get()];
-        if (address == GetAccountAddress(strOldAccount))
-            GetAccountAddress(strOldAccount, true);
-    }
+    // DS: setaccount returns an error if the address isn't in your wallet and wont create a new address
+    // Check if the address is in your wallet
+    if (!pwalletMain->mapAddressBook.count(address.Get()))
+        throw JSONRPCError(RPC_INVALID_ADDRESS_OR_KEY, "Invalid Electra address");
 
     pwalletMain->SetAddressBookName(address.Get(), strAccount);
 
@@ -913,7 +914,8 @@ Value ListReceived(const Array& params, bool fByAccounts)
         const CElectraAddress& address = item.first;
         const string& strAccount = item.second;
         map<CElectraAddress, tallyitem>::iterator it = mapTally.find(address);
-        if (it == mapTally.end() && !fIncludeEmpty)
+        // DS: fix listreceivedbyaddress returning sending addresses from address book
+        if ((it == mapTally.end() && !fIncludeEmpty) || !IsMine(*pwalletMain, address.Get()))
             continue;
 
         int64_t nAmount = 0;
@@ -938,6 +940,45 @@ Value ListReceived(const Array& params, bool fByAccounts)
             obj.push_back(Pair("amount",        ValueFromAmount(nAmount)));
             obj.push_back(Pair("confirmations", (nConf == std::numeric_limits<int>::max() ? 0 : nConf)));
             ret.push_back(obj);
+        }
+    }
+
+    // DS: RPC ListReceived will return change addresses not in the address book
+    // Add addresses from mapTally (this will include change addresses which aren't in the address book)
+    BOOST_FOREACH(const PAIRTYPE(CElectraAddress, tallyitem)& item, mapTally)
+    {        
+        const CElectraAddress& address = item.first;
+        const string& strAccount = "(change)";
+
+        map<CTxDestination, string>::iterator mi = pwalletMain->mapAddressBook.find(address.Get());
+        if (mi == pwalletMain->mapAddressBook.end()) {
+        
+            if (!IsMine(*pwalletMain, address.Get()))
+                continue;
+
+            int64_t nAmount = 0;
+            int nConf = std::numeric_limits<int>::max();
+            nAmount = item.second.nAmount;
+            nConf = item.second.nConf;
+
+            if (!fIncludeEmpty && nAmount == 0)
+                continue;
+
+            if (fByAccounts)
+            {
+                tallyitem& item = mapAccountTally[strAccount];
+                item.nAmount += nAmount;
+                item.nConf = min(item.nConf, nConf);
+            }
+            else
+            {
+                Object obj;
+                obj.push_back(Pair("address",       address.ToString()));
+                obj.push_back(Pair("account",       strAccount));
+                obj.push_back(Pair("amount",        ValueFromAmount(nAmount)));
+                obj.push_back(Pair("confirmations", (nConf == std::numeric_limits<int>::max() ? 0 : nConf)));
+                ret.push_back(obj);
+            }
         }
     }
 
