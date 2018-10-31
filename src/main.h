c7c06f12a7a7d6c9fcc41e3fb035a0221a3e9791
John <ComputerCraftr@users.noreply.github.com>
2018-10-31 15:14:04 -0700

    Bump wallet version and disconnect old peers

--- src/main.h
+++ src/main.h
@@ -58,7 +58,7 @@ struct CBlockTemplate;
 struct CNodeStateStats;
 
 /** Default for -blockmaxsize and -blockminsize, which control the range of sizes the mining code will create **/
-static const unsigned int DEFAULT_BLOCK_MAX_SIZE = 750000;
+static const unsigned int DEFAULT_BLOCK_MAX_SIZE = 7500000;
 static const unsigned int DEFAULT_BLOCK_MIN_SIZE = 0;
 /** Default for -blockprioritysize, maximum space for zero/low-fee transactions **/
 static const unsigned int DEFAULT_BLOCK_PRIORITY_SIZE = 50000;
