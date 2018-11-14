f955f28e5b6f3e6194ad7665f4fe8fb8954a9c4e
John <ComputerCraftr@users.noreply.github.com>
2018-11-13 17:42:07 -0700

    Upload GUI

---
+++ src/qt/zpivcontroldialog.h
@@ -0,0 +1,56 @@
+// Copyright (c) 2017-2018 The PIVX developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef ZPIVCONTROLDIALOG_H
+#define ZPIVCONTROLDIALOG_H
+
+#include <QDialog>
+#include <QTreeWidgetItem>
+#include "primitives/zerocoin.h"
+#include "privacydialog.h"
+
+class CZerocoinMint;
+class WalletModel;
+
+namespace Ui {
+class ZPivControlDialog;
+}
+
+class ZPivControlDialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit ZPivControlDialog(QWidget *parent);
+    ~ZPivControlDialog();
+
+    void setModel(WalletModel* model);
+
+    static std::set<std::string> setSelectedMints;
+    static std::set<CMintMeta> setMints;
+    static std::vector<CMintMeta> GetSelectedMints();
+
+private:
+    Ui::ZPivControlDialog *ui;
+    WalletModel* model;
+    PrivacyDialog* privacyDialog;
+
+    void updateList();
+    void updateLabels();
+
+    enum {
+        COLUMN_CHECKBOX,
+        COLUMN_DENOMINATION,
+        COLUMN_PUBCOIN,
+        COLUMN_VERSION,
+        COLUMN_CONFIRMATIONS,
+        COLUMN_ISSPENDABLE
+    };
+
+private slots:
+    void updateSelection(QTreeWidgetItem* item, int column);
+    void ButtonAllClicked();
+};
+
+#endif // ZPIVCONTROLDIALOG_H
