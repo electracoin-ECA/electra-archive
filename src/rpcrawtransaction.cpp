1c535d13f4999a4c1590613140e0aaa45852ea4a
Ruanne <ruanne.lloyd@multiply.co.za>
2019-01-02 21:06:52 +0200

    Rule change for atomic swap

--- src/rpcrawtransaction.cpp
+++ src/rpcrawtransaction.cpp
@@ -260,7 +260,7 @@ UniValue listunspent(const UniValue& params, bool fHelp)
         }
     }
 
-    int nWatchonlyConfig = 1;
+    int nWatchonlyConfig = 3;
     if(params.size() > 3) {
         nWatchonlyConfig = params[3].get_int();
         if (nWatchonlyConfig > 3 || nWatchonlyConfig < 1)
