f192d6747cb2987010871f1a56661c22afda09b5
Jenova7 <38404798+Jenova7@users.noreply.github.com>
2019-01-26 09:14:54 +0100

    Update chainparams.h

--- src/chainparams.h
+++ src/chainparams.h
@@ -72,9 +72,9 @@ public:
     bool SkipProofOfWorkCheck() const { return fSkipProofOfWorkCheck; }
     /** Make standard checks */
     bool RequireStandard() const { return fRequireStandard; }
-    //int64_t TargetTimespan() const { return nTargetTimespan; }
+    int64_t TargetTimespan() const { return nTargetTimespan; }
     int64_t TargetSpacing() const { return nTargetSpacing; }
-    //int64_t Interval() const { return nTargetTimespan / nTargetSpacing; }
+    int64_t Interval() const { return nTargetTimespan / nTargetSpacing; }
     int COINBASE_MATURITY() const { return nMaturity; }
     CAmount MaxMoneyOut() const { return nMaxMoneyOut; }
     /** The masternode count that we will allow the see-saw reward payments to be off by */
@@ -142,7 +142,7 @@ protected:
     int nEnforceBlockUpgradeMajority;
     int nRejectBlockOutdatedMajority;
     int nToCheckBlockUpgradeMajority;
-    //int64_t nTargetTimespan;
+    int64_t nTargetTimespan;
     int64_t nTargetSpacing;
     int nMandatoryUpgradeBlock;
     int nUpgradeBlockVersion;
